ee18c2414cf6036bce2d100162476263a8ff771e
Kyle Worrall <65330257+kylewhirl@users.noreply.github.com>
2024-02-26 12:50:45 -0800

    Update app.js

--- app.js
+++ app.js
@@ -132,39 +132,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-
-
-app.post('/incoming-sms', async (req, res) => {
-  const incomingMessage = req.body.Body;
-  const senderPhoneNumber = req.body.From;
-
-  console.log(`Received message from ${senderPhoneNumber}: ${incomingMessage}`);
-
-  // Process incoming message using ChatGPT API
-  try {
-    const chatGPTResponse = await axios.post(
-      chatGPTApiEndpoint,
-      { model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: `Respond to this message: ${incomingMessage}`}],
-        temperature: 0.7
-      },
-      { headers: { Authorization: `Bearer ${chatGPTApiKey}` } }
-    );
-
-    const chatGPTReply = chatGPTResponse.data.choices[0].message.content;
-
-    // Send ChatGPT reply via Twilio
-    client.messages.create({
-      body: chatGPTReply,
-      from: twilioPhoneNumber,
-      to: senderPhoneNumber
-    });
-
-    console.log(`Sent reply to ${senderPhoneNumber}: ${chatGPTReply}`);
-    res.sendStatus(200);
-  } catch (error) {
-    console.error('Error processing message:', error.message);
-    res.sendStatus(500);
-  }
-});
